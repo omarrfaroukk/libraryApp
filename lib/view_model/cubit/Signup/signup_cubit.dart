3b4448a1d3bdec3e2aed6d868d9fa64cd51c29fa
Omar <omarello2006@gmail.com>
2025-08-31 17:13:57 +0300

    Sign up changes

---
+++ lib/view_model/cubit/Signup/signup_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:library_app/view_model/cubit/auth_repo/auth.dart';
+import 'signup_state.dart';
+
+class SignupCubit extends Cubit<SignupState>{
+  SignupCubit(): super(SignupInitial());
+
+  Future<void>signup(String u, String p)async
+  {
+    emit(SignupInitial());
+    await Future.delayed(const Duration(seconds: 3));
+    if (Authentication.authedUsers.keys.contains(u)){
+      emit(SignupFailed());
+    }else{
+      Authentication.addUser(u,p);
+      emit(SignupSuccess());
+    }
+  }
+
+}
