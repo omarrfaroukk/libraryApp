d36a41aea7ff04f47354b3055e260bd0099d22b4
Omar <omarello2006@gmail.com>
2025-08-31 16:06:15 +0300

    Login Screen

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:library_app/data/models/my_classes.dart';
 import 'package:library_app/view/pages/home_page.dart';
+import 'package:library_app/view_model/cubit/Login/login_cubit.dart';
+import 'package:library_app/view_model/cubit/Login/login_state.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -18,120 +21,139 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          toolbarHeight: 80,
-          backgroundColor: const Color(0xFF5ABD8C),
-          centerTitle: true,
-          title: const Text(
-            "Booky",
-            style: TextStyle(
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w700,
-                fontSize: 32,
-                color: Colors.white,
-                fontStyle: FontStyle.italic),
-          ),
-        ),
-        body: Models.getBackgroundImage(Form(
-          key: _formKeyy,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Models.myCustomtextField("Username", false, TextInputType.text,
-                    usernameController, Icons.person_2_rounded),
-                const SizedBox(height: 20),
-                Column(
+    return BlocBuilder<Logincubit, LoginState>(
+      builder: (context, state) {
+        return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              toolbarHeight: 80,
+              backgroundColor: const Color(0xFF5ABD8C),
+              centerTitle: true,
+              title: const Text(
+                "Booky",
+                style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+            body: Models.getBackgroundImage(Form(
+              key: _formKeyy,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const Row(
+                    Models.myCustomtextField(
+                        "Username",
+                        false,
+                        TextInputType.text,
+                        usernameController,
+                        Icons.person_2_rounded),
+                    const SizedBox(height: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          child: Text(
-                            "Password",
-                            style: TextStyle(
-                                fontFamily: 'Roboto',
-                                fontSize: 20,
-                                color: Color(0xFF5ABD8C),
-                                fontWeight: FontWeight.w400),
-                          ),
+                        const Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 40),
+                              child: Text(
+                                "Password",
+                                style: TextStyle(
+                                    fontFamily: 'Roboto',
+                                    fontSize: 20,
+                                    color: Color(0xFF5ABD8C),
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ],
                         ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: TextFormField(
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: (value) {
+                                if (value!.isEmpty || value.length < 3) {
+                                  return "Please enter valid data";
+                                } else {
+                                  return null;
+                                }
+                              },
+                              controller: passwordController,
+                              keyboardType: TextInputType.text,
+                              obscureText: isVisible,
+                              obscuringCharacter: '*',
+                              decoration: InputDecoration(
+                                suffixIcon: IconButton(
+                                  icon: isVisible
+                                      ? const Icon(Icons.visibility_off)
+                                      : const Icon(Icons.visibility),
+                                  onPressed: () {
+                                    setState(() {
+                                      isVisible = !isVisible;
+                                    });
+                                  },
+                                ),
+                                icon: const Icon(Icons.password_rounded),
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                    borderSide: const BorderSide(
+                                        color: Color(0xFF5ABD8C), width: 2)),
+                              )),
+                        )
                       ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: TextFormField(
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (value) {
-                            if (value!.isEmpty || value.length < 3) {
-                              return "Please enter valid data";
-                            } else {
-                              return null;
-                            }
-                          },
-                          controller: passwordController,
-                          keyboardType: TextInputType.text,
-                          obscureText: isVisible,
-                          obscuringCharacter: '*',
-                          decoration: InputDecoration(
-                            suffixIcon:
-                               IconButton(icon:isVisible? const Icon(Icons.visibility_off) :const Icon(Icons.visibility), onPressed: () {
-                                  setState(() {
-                                    isVisible = !isVisible;
-                                  });
-                                },),
-                            icon: const Icon(Icons.password_rounded),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
-                                borderSide: const BorderSide(
-                                    color: Color(0xFF5ABD8C), width: 2)),
-                          )),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  "Forgot your Password? ",
-                  style: TextStyle(fontSize: 18, color: Color(0xFF707070)),
-                ),
-                const SizedBox(height: 20),
-                MaterialButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    side: BorderSide(
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Forgot your Password? ",
+                      style: TextStyle(fontSize: 18, color: Color(0xFF707070)),
+                    ),
+                    const SizedBox(height: 20),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        side: BorderSide(
+                          color: (isValidP && isValidU)
+                              ? const Color(0xFF5ABD8C)
+                              : const Color(0xFFAFDFC7),
+                          width: 2,
+                        ),
+                      ),
+                      minWidth: 207,
+                      height: 50,
                       color: (isValidP && isValidU)
                           ? const Color(0xFF5ABD8C)
                           : const Color(0xFFAFDFC7),
-                      width: 2,
+                      onPressed: () {
+                        if (_formKeyy.currentState!.validate()) {
+                          if (LoginState is LoginSuccess) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()));
+                          } else if (LoginState is LoginFailed) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text("User already exists")));
+                          }
+                        }
+                      },
+                      child: const Text(
+                        "Log in",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontSize: 18),
+                      ),
                     ),
-                  ),
-                  minWidth: 207,
-                  height: 50,
-                  color: (isValidP && isValidU)
-                      ? const Color(0xFF5ABD8C)
-                      : const Color(0xFFAFDFC7),
-                  onPressed: () {
-                    if (_formKeyy.currentState!.validate()) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomePage()));
-                    }
-                  },
-                  child: const Text(
-                    "Log in",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 18),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        )));
+              ),
+            )));
+      },
+    );
   }
 }
